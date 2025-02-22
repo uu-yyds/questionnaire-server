79084d2ae2bbc23c57e8ac2e0ab9ab0a7e11afbd
zhangsiyu11 <zhangsiyu11@kuaishou.com>
2025-02-22 17:17:48 +0800

    feat: 试图增加cookie

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './transform/transform.interceptor';
 import { HttpExceptionFilter } from './http-exception/http-exception.filter';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,7 @@ async function bootstrap() {
   app.useGlobalInterceptors(new TransformInterceptor()); // 使用全局拦截器
   app.useGlobalFilters(new HttpExceptionFilter()); // 使用全局过滤器
   app.enableCors(); // 启用CORS
+  // app.use(cookieParser());
   await app.listen(process.env.PORT ?? 3005);
 }
 
